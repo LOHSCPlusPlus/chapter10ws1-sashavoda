b3fbadaf485f05847b042ad3987f934f56b107a3
SASHAVODA2 <>
2022-03-09 17:14:04 +0000

    These three are done

--- Point.cpp
+++ Point.cpp
@@ -1,9 +1,35 @@
 #include "Point.h"
-
+#include <math.h>
+#include <iostream>
+using namespace std;
 // Notice how when we use field names in the functions that
 // belong to Point, we can just use the name without a dot operator.
-Point::Point() {
-    x = 0.0;
-    y = 0.0;
+Point::Point(double xVal, double yVal){
+    x = xVal;
+    y = yVal;
 }
+Point::Point(){
+    x = 0;
+    y = 0;
+}
+
+//double Point::getX() {
+  //  return x;}
+//double Point::getY(){
+ // return y;}
+//void Point::setX(double xVal) {
+   // x = xVal;}
+//void Point::setY(double yVal){
+ //y = yVal;}
+void Point::print() {
+   cout << "x: " << x;
+cout << "y: " << y;
+	cout << endl;
+  } 
+ double Point::calcDistance(Point other) {
+double xVal = pow(x - other.x, 2);
+double yVal = pow(y - other.y, 2);
+double dist = sqrt(xVal + yVal);
+return dist;
+  }
 
--- Point.h
+++ Point.h
@@ -1,6 +1,13 @@
 class Point{
-    public:
-        Point();
+  public:
+    Point();
+      void print();
+    double calcDistance(Point other);
+    // double getX();
+     // double getY();
+//void setX( double xVal);
+//void setY( double yVal);
+   Point(double xVal, double yVal);
     private:
         double x;
         double y;
